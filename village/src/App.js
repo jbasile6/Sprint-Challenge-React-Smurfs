677efe7f8c559cdc645ca3ca3bf0fd832ac90d9d
James Basile <jbasile14@gmail.com>
2019-03-01 12:26:54 -0500

    created two different Routes for smurfs and smurf form

--- village/src/App.js
+++ village/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { Route } from 'react-router-dom';
 
 import './App.css';
 import SmurfForm from './components/SmurfForm';
@@ -9,20 +10,14 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      smurfs: [
-        {
-          name: '',
-          age: '',
-          height: ''
-        }
-      ],
+      smurfs: [],
     };
   }
 
   componentDidMount() {
     axios.get('http://localhost:3333/smurfs')
-    .then( res => this.setState({ smurfs: res.data }))
-    .catch( err => console.log(err));
+      .then(res => this.setState({ smurfs: res.data }))
+      .catch(err => console.log(err));
   }
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
@@ -30,8 +25,14 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <SmurfForm smurfs={this.state.smurfs} />
-        <Smurfs smurfs={this.state.smurfs} />
+        <Route 
+        path='/smurfform'
+        render= {
+          props => <SmurfForm {...props} smurfs={this.state.smurfs} />} />
+        <Route 
+        path='/'  
+        render={
+          props => < Smurfs {...props} smurfs={this.state.smurfs} />} />
       </div>
     );
   }
